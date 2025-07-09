70c614496fb4e5b26317d7404ffdd1858a010821
Jason Wolfe <jason06051998@gmail.com>
2025-07-08 18:50:32 -0700

    Preparing for deployment

--- config.py
+++ config.py
@@ -7,6 +7,12 @@ class Config:
     SECRET_KEY = os.getenv("SECRET_KEY", "fallback_dev_key")
     SQLALCHEMY_DATABASE_URI = os.getenv("SQLALCHEMY_DATABASE_URI", "sqlite:///db.sqlite3")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SESSION_COOKIE_SAMESITE = "lax"
-    SESSION_COOKIE_SECURE = False
     SESSION_COOKIE_HTTPONLY = True
+    SESSION_COOKIE_SAMESITE = os.getenv("SESSION_COOKIE_SAMESITE", "Lax")
+    SESSION_COOKIE_SECURE = os.getenv("SESSION_COOKIE_SECURE", "False").lower() == "true"
+
+class DevelopmentConfig(Config):
+    DEBUG = True
+
+class ProductionConfig(Config):
+    DEBUG = False
